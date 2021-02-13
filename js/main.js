bae154984656ef370e8444f494c6cd42a3df3b0a
lekomtsevdo <65308118+lekomtsevdo@users.noreply.github.com>
2021-02-13 23:57:15 +0500

    Update main.js
    Создание второй функции.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
-  if (min < 0) {
-		console.log ('Минимум меньше допустимого значения');
-		return;
-	}
-  if (min === 0) {
-		console.log ('Минимальное значение равно нулю');
-	}
+  if ((min < 0)||(min > max)||(min === max)) {
+      console.log ('Указан некорректный диапазон!');
+      return;
+  }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
+function getRandomFraction(min, max, numberCharacters) {
+  if ((min < 0)||(min > max)||(min === max)||(numberCharacters < 0)) {
+      console.log ('Указан некорректный диапазон!');
+      return;
+  }
+  let number = Math.random() * (max - min) + min; //Максимум не включается, минимум включается, без округления
+  return number.toFixed(numberCharacters); //Округление с плавающей точкой
 }
