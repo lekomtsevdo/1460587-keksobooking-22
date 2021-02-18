192d30412d5ca7ab3510a1dd09773bf54f0a3dda
lekomtsevdo <65308118+lekomtsevdo@users.noreply.github.com>
2021-02-18 21:14:16 +0500

    Update main.js
    Устранение недочетов eslint-ом.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
   if ((min < 0)||(min > max)||(min === max)) {
-      console.log ('Указан некорректный диапазон!');
-      return;
+  // console.log ('Указан некорректный диапазон!');
+    return;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -11,9 +11,12 @@ function getRandomInt(min, max) {
 
 function getRandomFraction(min, max, numberCharacters) {
   if ((min < 0)||(min > max)||(min === max)||(numberCharacters < 0)) {
-      console.log ('Указан некорректный диапазон!');
-      return;
+  // console.log ('Указан некорректный диапазон!'); 
+    return;
   }
   let number = Math.random() * (max - min) + min; //Максимум не включается, минимум включается, без округления
   return number.toFixed(numberCharacters); //Округление с плавающей точкой
 }
+
+getRandomInt();
+getRandomFraction();
